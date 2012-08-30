ec7c9e6f08f130965b7d7db3a6318f5b07e2edc9
James Ellis <j@athleticsnyc.com>
2012-08-30 00:36:53 -0400

    Initial import of demo

---
+++ demo/js/athletics_infostory.js
@@ -0,0 +1,249 @@
+/*
+ * 		Athletics Infostory js module:
+ * 			infostory.js
+ *
+ *		desc:
+ * 		
+ * 		requires:
+ * 			jQuery
+ */
+
+var athletics = (function( app, $ ) {
+	
+	/* define new module */
+	app.infostory = (function($){
+		
+		// private vars
+		var _initialized = false,
+			$obj = null,
+			$bgs = null,
+			$controls = null,
+			$border = null,
+			$credit = null;
+			
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _init() {
+
+			$obj = $('div.athletics.infostory[data-infostory-name="demo"]');
+			
+			if ($obj.length != 1) return false;
+			
+			_init_controls();
+			_init_bgs();
+			_init_border();
+			_init_credit();
+			
+			// basic styling
+			$obj.css({
+				'position' : 'relative',
+				'width' : $obj.data('width'),
+				'height' : $obj.data('height'),
+				'font-size' : 0
+			});
+			
+			$obj.show();
+			
+			_initialized = true;
+
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _init_border() {
+			
+			if (!$obj.data('use-border')) return false;
+			
+			$border = $obj.find('div.infostory_border');
+			
+			$border.css({
+				'border' : '1px solid #ccc',
+				'width' : $obj.data('width') - 2,
+				'height' : $obj.data('height') - 2,
+				'position' : 'absolute',
+				'top' : 0,
+				'left' : 0,
+				'z-index' : 2
+			});
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _init_controls() {
+			
+			$controls = $obj.find('div.infostory_controls');
+			
+			// basic styling
+			$controls.css({
+				'position' : 'absolute',
+				'z-index' : 4,
+				'top' : 20,
+				'left' : 20,
+				'font-size' : '14px'
+			});
+			
+			// general styling on controls
+			$controls.find('div.option').css({
+				'color' : '#ffffff',
+				'font-size' : '12px',
+				'font-family' : 'Arial',
+				'font-weight' : 'bold',
+				'cursor' : 'pointer'
+			});
+			
+			$controls.find('div.option span.label').css({
+				'padding' : '3px 10px',
+				'display' : 'block'
+			});
+			
+			// position controls
+			$controls.find('div.option').css({
+				'float' : 'left',
+				'margin-right' : '5px'
+			});
+			
+			// attach events
+			$controls.find('div.option').on('click',function(){
+				var $this = $(this);
+				_change_bg(
+					$this.data('id')
+				);
+			});
+			
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _init_bgs() {
+			
+			$bgs = $obj.find('div.infostory_backgrounds');
+			
+			$bgs.find('span.bg').each(function(){
+
+				var $this = $(this),
+					bg_html = '';
+				
+				if (!$this.hasClass('bg_initialized')) {
+					
+					bg_html += '<img src="'+ $this.data('img-src') +'" alt="">';
+					
+					$this
+						.html( bg_html )
+						.addClass('bg_initialized');
+				}
+			});
+			
+			// add styling
+			$bgs.css({
+				'position' : 'absolute',
+				'z-index' : 1,
+				'top' : 0,
+				'left' : 0
+			});
+			
+			$bgs.find('span.bg').css({
+				'position' : 'absolute',
+				'top' : 0,
+				'left' : 0
+			});
+			
+			// reveal default bg
+			_change_bg(
+				$bgs.find('span.bg.default').data('id')
+			);
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _change_bg( id ) {
+			
+			$bgs.find('span.bg')
+				.show()
+				.stop()
+				.animate({
+					'opacity' : 0
+				},{
+					'duration' : 300
+				});
+			
+			var $target = $bgs.find('span.bg[data-id="'+ id +'"]');
+			
+			$target
+				.show()
+				.stop()
+				.css('opacity',0)
+				.animate({
+					'opacity' : 1
+				},{
+					'duration' : 300
+				});
+			
+			_change_bg_control( id );
+			
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _reset_bg_controls() {
+			$controls.find('div.option').css({
+				'background-color' : 'black'
+			});
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _change_bg_control( id ) {
+			
+			_reset_bg_controls();
+			
+			var $target = $controls.find('div.option[data-id="'+ id +'"]');
+			
+			$target.css({
+				'background-color' : 'blue'
+			});
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		
+		function _init_credit() {
+			
+			$credit = $obj.find('div.infostory_credit');
+			
+			$credit.css({
+				'width' : $obj.data('width'),
+				'height' : 30,
+				'background-color' : '#cccccc',
+				'position' : 'absolute',
+				'left' : 0,
+				'bottom' : 0,
+				'z-index' : 3,
+				'text-align' : 'right',
+				'font-family' : 'Helvetica Neue, Helvetica, Arial',
+				'font-size' : '12px',
+				'line-height' : '18px'
+			});
+			
+			$credit.find('span.infostory_copy').css({
+				'display' : 'block',
+				'padding-top' : '5px',
+				'padding-right' : '10px'
+			});
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		return {
+			init : _init
+		};
+		
+	}($));
+	
+	return app; /* return augmented app object */
+	
+}( athletics || {}, jQuery )); /* import app if exists, or create new; import jQuery */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+jQuery(document).ready(function(){
+	athletics.infostory.init();
+});
